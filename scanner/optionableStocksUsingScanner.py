6809950836ea9befd5933e4c6178b42f0bb341e4
Pavan Mullapudy <pavan.mullapudy@gmail.com>
2020-09-12 12:49:26 +0530

    Revert "Update optionableStocksUsingScanner.py"
    This reverts commit 9d089e23bd6dbcc8e7b88be3104d33ea1097d68b.

--- scanner/optionableStocksUsingScanner.py
+++ scanner/optionableStocksUsingScanner.py
@@ -12,6 +12,7 @@ import time
 import threading
 import datetime
 import sys
+
 import pandas as pd
 
 class StockScanner(EWrapper, EClient):
@@ -30,12 +31,14 @@ class StockScanner(EWrapper, EClient):
     
     @iswrapper
     def scannerData(self, reqId, rank, details, distance, benchmark, projection, legsStr):
+
         # Print the symbols in the returned results
         print('{}: {} : {}'.format(rank, details.contract.symbol, details.contract.secType))
         self.count += 1
         
     @iswrapper
     def scannerDataEnd(self, reqId):
+    
         # Print the number of results
         print('Number of results: {}'.format(self.count))
     
@@ -47,24 +50,36 @@ def main():
     # Create the client and connect to TWS
     client = StockScanner('127.0.0.1', 7497, 7)
     time.sleep(3)
+    
+    '''
+    Note 1: Retreiving directly the stocks and using filter "hasOptionsIs" dont seem to work.
+    Note 2: A beter approach is to retreive the Single Stock Futures.
 
     # Create the object ScannerSubscription
     scanSub = ScannerSubscription() # Defines a market scanner request
     scanSub.instrument = 'STOCK.HK'
     scanSub.locationCode = 'STK.HK.NSE'
-    # scanSub.scanCode = 'ALL_SYMBOLS_ASC'
-    scanSub.scanCode = 'TOP_TRADE_COUNT'
-
+    scanSub.scanCode = 'ALL_SYMBOLS_ASC'
+    
     # Set additional filter criteria
     tagvalues = []
-    tagvalues.append(TagValue('hasOptionsIs', 'true'))
-    tagvalues.append(TagValue("usdPriceAbove", "0"))
-    tagvalues.append(TagValue("usdPriceBelow", "2"))
+    tagvalues.append(TagValue('avgOptVolumeAbove', '0')) # This dont seem to give desired result !!!
+    tagvalues.append(TagValue('hasOptionsIs', 'Yes')) # This dont seem to give desired result !!!
     print(tagvalues)
 
     # Request the scanner subscription
     client.reqScannerSubscription(7, scanSub, [], tagvalues)
+    '''
 
+    # Create the object ScannerSubscription
+    scanSub = ScannerSubscription() # Defines a market scanner request
+    scanSub.instrument = 'SSF.HK' # SSF = Single Stock Futures
+    scanSub.locationCode = 'SSF.HK.NSE'
+    scanSub.scanCode = 'ALL_SYMBOLS_ASC'
+    scanSub.abovePrice = '500'
+
+    # Request the scanner subscription
+    client.reqScannerSubscription(7, scanSub, [], [])
     # Disconnect from TWS
     time.sleep(5)
     client.disconnect()
